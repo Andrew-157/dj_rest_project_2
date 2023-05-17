dd1503534fd327a355f2ee597de83d63c818ace5
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-17 11:50:58 +0300

    Created model "Story"

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Story(models.Model):
+    title = models.CharField(max_length=155)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-pub_date']
