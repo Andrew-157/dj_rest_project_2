6b84b2c67ac189e4233f2aed205f2ea7511a0685
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-23 13:13:35 +0300

    Update admin.py

--- stories/admin.py
+++ stories/admin.py
@@ -11,6 +11,7 @@ from stories.models import Story, Review, Rating
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'date_joined',
                     'is_superuser', 'is_active', 'is_staff']
+    list_filter = ['username', 'date_joined']
     search_fields = ['username']
 
 
