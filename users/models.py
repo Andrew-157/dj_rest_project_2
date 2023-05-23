aba4a7dc6ef4a6b5c116aeb448100d639a84f614
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-23 14:37:47 +0300

    Added validation of Image Size

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from users.validators import validate_file_size
 
 
 class CustomUser(AbstractUser):
-    image = models.ImageField(upload_to='users/images')
+    image = models.ImageField(upload_to='users/images',
+                              validators=[validate_file_size])
 
     class Meta:
         ordering = ['id']
