e1f6c9d2412709c2b420b6fbb425a1db5f279a25
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-23 09:52:09 +0300

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer,\
     UserSerializer as BaseUserSerializer
+from rest_framework import serializers
+from users.models import CustomUser
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
@@ -9,6 +11,12 @@ class UserCreateSerializer(BaseUserCreateSerializer):
             'id', 'username', 'email', 'password', 'image'
         ]
 
+    def validate_email(self, value):
+        # check if user enters email that is not in database
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Duplicate email')
+        return value
+
 
 class UserSerializer(BaseUserSerializer):
 
@@ -16,3 +24,12 @@ class UserSerializer(BaseUserSerializer):
         fields = [
             'id', 'username', 'email', 'image'
         ]
+
+    def validate_email(self, value):
+        # check if user enters email that is either their
+        # existing email or new email that is not in the database
+        current_user = self.context['request'].user
+        user_with_email = CustomUser.objects.filter(email=value).first()
+        if user_with_email and user_with_email != current_user:
+            raise serializers.ValidationError('Duplicate email')
+        return value
