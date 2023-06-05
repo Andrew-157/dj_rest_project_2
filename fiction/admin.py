cc11084261ffc05a8d05b7df06ac8be197b26f42
Andrew Subotin <subotinandrey5@gmail.com>
2023-06-05 12:53:35 +0300

    Created model "Category"

--- fiction/admin.py
+++ fiction/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from fiction.models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'slug'
+    ]
+    list_filter = ['title', 'slug']
+    search_fields = ['title', 'slug']
